024923edd970024ff045d28deccdb353133cc341
Adrian PK <adrianpk@adrianpk.com>
2020-02-06 15:06:02 +0100

    Event store setup.

--- config/test.exs
+++ config/test.exs
@@ -8,6 +8,16 @@ config :ledger, Ledger.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+# Configure the event store database
+config :ledger, Ledger.EventStore,
+  serializer: Commanded.Serialization.JsonSerializer,
+  types: EventStore.PostgresTypes,
+  username: "ledger",
+  password: "ledger",
+  database: "ledger_eventstore_test",
+  hostname: "localhost",
+  pool_size: 10
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :ledger, LedgerWeb.Endpoint,
