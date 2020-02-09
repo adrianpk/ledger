3d79b51a2ee7d4ac28bd239de0052bfd1ca7af5a
Adrian K <adrian.git@kuguar.dev>
2020-02-09 19:50:43 +0100

    Tracking status projection.

---
+++ lib/ledger/warehouse/projectors/tracking_status.ex
@@ -0,0 +1,20 @@
+defmodule Conduit.Warehouse.Projectors.TrackingStatus do
+  use Commanded.Projections.Ecto,
+    application: Ledger.Warehouse,
+    name: "Warehouse.Projectors.TrackingStatus"
+  alias Ledger.Warehouse.Events.ReceivedFromTransport
+  alias Ledger.Warehouse.Projections.TrackingStatus
+
+  project %ReceivedFromTransport{} = received do
+    Ecto.Multi.insert(multi, :tracking_status, %TrackingStatus{
+      uuid: received.tracking_uuid,
+      package_uuid: received.package_uuid,
+      vehicle_uuid: received.vehicle_uuid,
+      driver_uuid: received.driver_uuid,
+      warehouse_uuid: received.warehouse_uuid,
+      gate_uuid: received.gate_uuid,
+      notes: received.notes,
+      tags: received.tags
+    })
+  end
+end
